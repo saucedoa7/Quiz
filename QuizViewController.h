a3dc1fb4d082ef6055ebfffacd4e2e92d6ade764
Albert Saucedo <saucedoa7@gmail.com>
2014-09-15 21:07:36 -0500

    Connected Labels and Buttons

---
+++ QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  Quiz
+//
+//  Created by Albert Saucedo on 9/15/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
---
+++ QuizViewController.m
@@ -0,0 +1,37 @@
+//
+//  QuizViewController.m
+//  Quiz
+//
+//  Created by Albert Saucedo on 9/15/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+#import "QuizViewController.h"
+
+@interface QuizViewController ()
+
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
+@end
+
+@implementation QuizViewController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+
+-(IBAction)showQuestion:(id)sender){
+
+}
+
+-(IBAction)showAnswer:(id)sender){
+    
+}
+
+@end
